ca5f67492fc62bfb3218963ba99ff670618f2db1
Jhonatan Mejia <sistemas@mejoraysoluciones.com>
2019-05-01 21:01:20 -0500

    Inicializa proyecto

---
+++ Arq2Corte/Arq2Corte-ejb/src/java/SessionBean/calcbeanLocal.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SessionBean;
+
+import javax.ejb.Local;
+
+/**
+ *
+ * @author quenan
+ */
+@Local
+public interface calcbeanLocal {
+
+    Integer addition(int a, int b);
+
+    
+}
